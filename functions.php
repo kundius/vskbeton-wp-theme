b4714fe0afcd5ae7697c0ac02af1283aa40acf20
Kundius Ruslan <kundius.ruslan@gmail.com>
2023-12-15 00:36:33 +0300

    1

--- functions.php
+++ functions.php
@@ -202,33 +202,33 @@ function current_type_nav_class($classes, $item) {
 
 add_filter('wpcf7_autop_or_not', '__return_false');
 
-remove_filter( 'sanitize_title', 'sanitize_title_with_dashes' );
-add_filter( 'sanitize_title', 'wpse5029_sanitize_title_with_dashes' );
-function wpse5029_sanitize_title_with_dashes($title) {
-    $title = strip_tags($title);
-    // Preserve escaped octets.
-    $title = preg_replace('|%([a-fA-F0-9][a-fA-F0-9])|', '---$1---', $title);
-    // Remove percent signs that are not part of an octet.
-    $title = str_replace('%', '', $title);
-    // Restore octets.
-    $title = preg_replace('|---([a-fA-F0-9][a-fA-F0-9])---|', '%$1', $title);
+// remove_filter( 'sanitize_title', 'sanitize_title_with_dashes' );
+// add_filter( 'sanitize_title', 'wpse5029_sanitize_title_with_dashes' );
+// function wpse5029_sanitize_title_with_dashes($title) {
+//     $title = strip_tags($title);
+//     // Preserve escaped octets.
+//     $title = preg_replace('|%([a-fA-F0-9][a-fA-F0-9])|', '---$1---', $title);
+//     // Remove percent signs that are not part of an octet.
+//     $title = str_replace('%', '', $title);
+//     // Restore octets.
+//     $title = preg_replace('|---([a-fA-F0-9][a-fA-F0-9])---|', '%$1', $title);
 
-    $title = remove_accents($title);
-    if (seems_utf8($title)) {
-        //if (function_exists('mb_strtolower')) {
-        //    $title = mb_strtolower($title, 'UTF-8');
-        //}
-        $title = utf8_uri_encode($title, 200);
-    }
+//     $title = remove_accents($title);
+//     if (seems_utf8($title)) {
+//         //if (function_exists('mb_strtolower')) {
+//         //    $title = mb_strtolower($title, 'UTF-8');
+//         //}
+//         $title = utf8_uri_encode($title, 200);
+//     }
 
-    //$title = strtolower($title);
-    $title = preg_replace('/&.+?;/', '', $title); // kill entities
-    $title = str_replace('.', '-', $title);
-    // Keep upper-case chars too!
-    $title = preg_replace('/[^%a-zA-Z0-9 _-]/', '', $title);
-    $title = preg_replace('/\s+/', '-', $title);
-    $title = preg_replace('|-+|', '-', $title);
-    $title = trim($title, '-');
+//     //$title = strtolower($title);
+//     $title = preg_replace('/&.+?;/', '', $title); // kill entities
+//     $title = str_replace('.', '-', $title);
+//     // Keep upper-case chars too!
+//     $title = preg_replace('/[^%a-zA-Z0-9 _-]/', '', $title);
+//     $title = preg_replace('/\s+/', '-', $title);
+//     $title = preg_replace('|-+|', '-', $title);
+//     $title = trim($title, '-');
 
-    return $title;
-}
+//     return $title;
+// }
