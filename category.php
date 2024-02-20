8c10487b989d26077d20fbb38bd23405d3af6491
Kundius Ruslan <kundius.ruslan@gmail.com>
2024-02-20 11:04:54 +0000

    1

--- category.php
+++ category.php
@@ -12,7 +12,7 @@
       <main class="main">
         <div class="container">
           1234
-          <?php if (have_posts()): ?>
+          <?php wp_reset_postdata(); if (have_posts()): ?>
             <div class="document">
               <h1 class="document__title"><?php the_title() ?></h1>
               <div class="document__content">
