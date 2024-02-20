c7a378694a8f6d8362f02a04ff9e55bb2e2a15af
Kundius Ruslan <kundius.ruslan@gmail.com>
2024-02-20 11:02:07 +0000

    1

---
+++ category.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes();?> itemscope itemtype="http://schema.org/WebSite">
+  <head>
+    <?php get_template_part('partials/head');?>
+  </head>
+  <body <?php body_class() ?>>
+    <?php wp_body_open() ?>
+
+    <div class="page">
+      <?php get_template_part('partials/header') ?>
+      
+      <main class="main">
+        <div class="container">
+          <?php if (have_posts()): ?>
+            <div class="document">
+              <h1 class="document__title"><?php the_title() ?></h1>
+              <div class="document__content">
+                <?php the_content() ?>
+              </div>
+            </div>
+          <?php else : ?>
+          Результатов не найдено
+          <?php endif; ?>
+        </div>
+      </main>
+    
+      <?php get_template_part('partials/footer') ?>
+    </div>
+
+    <?php wp_footer() ?>
+  </body>
+</html>
--- inc/post-types.php
+++ inc/post-types.php
@@ -1,31 +1,31 @@
 <?php
 
-function register_cpts() {
-	// News
-	$labels0 = array(
-		'name' => 'Товары',
-		'singular_name' => 'Товар', // Добавить->Запись (Именительный падеж)
-		'add_new' => 'Добавить товар', // в меню и на кнопке
-		'add_new_item' => 'Добавить товар', // заголовок
-		'edit_item' => 'Редактировать товар',
-		'new_item' => 'Новый товар',
-		'all_items' => 'Все товары', // подменю в админке
-		'view_item' => 'Просмотреть товар',
-		'search_items' => 'Поиск товаров',
-		'not_found' =>  'Товаров не найдено.',
-		'not_found_in_trash' => 'Товаров в корзине не найдено.',
-		'menu_name' => 'Товары' // ссылка в меню в админке
-	);
-	$args = array(
-		'labels' => $labels0,
-		'public' => true,
-		'show_ui' => true, // показывать интерфейс в админке
-		'has_archive' => true,
-		'menu_icon' => 'dashicons-text-page',
-		'menu_position' => 20, // порядок в меню
-		'taxonomies' => array(),
-		'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'custom-fields', 'revisions', 'post-formats')
-	);
-	register_post_type('product', $args);
-}
-add_action('init', 'register_cpts');
+// function register_cpts() {
+// 	// News
+// 	$labels0 = array(
+// 		'name' => 'Товары',
+// 		'singular_name' => 'Товар', // Добавить->Запись (Именительный падеж)
+// 		'add_new' => 'Добавить товар', // в меню и на кнопке
+// 		'add_new_item' => 'Добавить товар', // заголовок
+// 		'edit_item' => 'Редактировать товар',
+// 		'new_item' => 'Новый товар',
+// 		'all_items' => 'Все товары', // подменю в админке
+// 		'view_item' => 'Просмотреть товар',
+// 		'search_items' => 'Поиск товаров',
+// 		'not_found' =>  'Товаров не найдено.',
+// 		'not_found_in_trash' => 'Товаров в корзине не найдено.',
+// 		'menu_name' => 'Товары' // ссылка в меню в админке
+// 	);
+// 	$args = array(
+// 		'labels' => $labels0,
+// 		'public' => true,
+// 		'show_ui' => true, // показывать интерфейс в админке
+// 		'has_archive' => true,
+// 		'menu_icon' => 'dashicons-text-page',
+// 		'menu_position' => 20, // порядок в меню
+// 		'taxonomies' => array(),
+// 		'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'custom-fields', 'revisions', 'post-formats')
+// 	);
+// 	register_post_type('product', $args);
+// }
+// add_action('init', 'register_cpts');
