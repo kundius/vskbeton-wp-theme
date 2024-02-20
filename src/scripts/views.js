30eb504a29716e32a6ab21f1ad2dfcda1915b54b
Kundius Ruslan <kundius.ruslan@gmail.com>
2024-02-20 19:49:57 +0000

    deploy

--- src/scripts/views.js
+++ src/scripts/views.js
@@ -1,8 +1,8 @@
 const elements = document.querySelectorAll('[data-views]') || []
 elements.forEach(async (el) => {
   const searchParams = new URLSearchParams()
-  searchParams.set('id', el.dataset.id)
-  if (el.dataset.increase) {
+  searchParams.set('id', el.dataset.views)
+  if (el.dataset.viewsIncrease) {
     searchParams.set('increase', 1)
   }
   const response = await fetch(`${theme_ajax.url}?${searchParams.toString()}`)
