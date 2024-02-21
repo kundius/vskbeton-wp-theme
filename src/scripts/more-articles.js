949e154f74559710f7a2a455e297598c3b414c5a
Kundius Ruslan <kundius.ruslan@gmail.com>
2024-02-21 00:57:17 +0000

    deploy

--- src/scripts/more-articles.js
+++ src/scripts/more-articles.js
@@ -1,34 +1,32 @@
-async function init() {
-  const list = document.querySelector("#more-articles-list");
-  const button = document.querySelector("#more-articles-button");
+const list = document.querySelector("#more-articles-list");
+const button = document.querySelector("#more-articles-button");
 
-  if (!(button && list)) return;
+if (button && list) {
+  button.addEventListener("click", async () => {
+    const searchParams = new URLSearchParams();
+    searchParams.set("action", "more_articles");
+    searchParams.set("query", posts_vars);
+    searchParams.set("page", current_page);
 
-  const searchParams = new URLSearchParams();
-  searchParams.set("action", "more_articles");
-  searchParams.set("query", posts_vars);
-  searchParams.set("page", current_page);
+    let text = button.innerHTML;
 
-  let text = button.innerHTML;
+    button.innerHTML = "Загрузка...";
 
-  button.innerHTML = "Загрузка...";
+    const response = await fetch(theme_ajax.url, {
+      method: "POST",
+      body: searchParams,
+    });
+    const data = await response.json();
 
-  const response = await fetch(theme_ajax.url, {
-    method: "POST",
-    body: searchParams,
-  });
-  const data = await response.json();
-
-  if (response.ok) {
-    button.innerHTML = text;
-    div.insertAdjacentHTML("beforeend", data);
-    list.current_page++;
-    if (current_page == max_pages) {
+    if (response.ok) {
+      button.innerHTML = text;
+      div.insertAdjacentHTML("beforeend", data);
+      list.current_page++;
+      if (current_page == max_pages) {
+        button.setAttribute("hidden", true);
+      }
+    } else {
       button.setAttribute("hidden", true);
     }
-  } else {
-    button.setAttribute("hidden", true);
-  }
+  });
 }
-
-init();
