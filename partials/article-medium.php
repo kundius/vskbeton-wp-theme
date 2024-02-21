b093d0779a1f774b1f7f78e70a9762a1378735e6
Kundius Ruslan <kundius.ruslan@gmail.com>
2024-02-21 09:45:17 +0000

    deploy

--- partials/article-medium.php
+++ partials/article-medium.php
@@ -1,23 +1,25 @@
-<article class="articles-medium">
-  <figure class="articles-medium__image">
+<article class="articles-card">
+  <figure class="articles-card__image">
     <img src="<?php the_post_thumbnail_url('article-medium') ?>" alt="<?php the_title() ?>" />
     <?php if ($time = get_field('reading_time')): ?>
-      <div class="articles-medium__time">
+      <div class="articles-card__time">
         <?php echo $time ?>
       </div>
     <?php endif ?>
   </figure>
 
-  <div class="articles-medium__meta">
-    <div class="articles-medium__date">
-      <?php echo get_the_date('j.m.Y') ?>
+  <div class="articles-card__content">
+    <div class="articles-card__meta">
+      <div class="articles-card__date">
+        <?php echo get_the_date('j.m.Y') ?>
+      </div>
+      <div class="articles-card__views" data-views="<?php echo get_the_ID() ?>">...</div>
     </div>
-    <div class="articles-medium__views" data-views="<?php echo get_the_ID() ?>">...</div>
-  </div>
 
-  <div class="articles-medium__title">
-    <a href="<?php the_permalink() ?>">
-      <?php the_title() ?>
-    </a>
+    <div class="articles-card__title">
+      <a href="<?php the_permalink() ?>">
+        <?php the_title() ?>
+      </a>
+    </div>
   </div>
 </article>
