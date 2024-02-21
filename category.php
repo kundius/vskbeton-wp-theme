2e0147224bd3b12bdcfeb3cca8af6104e1f2ee07
Kundius Ruslan <kundius.ruslan@gmail.com>
2024-02-21 01:30:12 +0000

    deploy

--- category.php
+++ category.php
@@ -1,21 +1,45 @@
 <?php
-// $category = get_queried_object();
-// $query_params = [
-//   'post_type' => 'post',
-//   'posts_per_page' => 3,
-//   'order' => 'DESC',
-//   'orderby' => 'date',
-//   'paged' => get_query_var('paged') ?: 1,
-//   'tax_query' => [
-//     'relation' => 'OR',
-//     [
-//       'taxonomy' => $category->taxonomy,
-//       'field' => 'id',
-//       'terms' => [$category->term_id]
-//     ]
-//   ]
-// ];
-// $query = new WP_Query($query_params);
+$category = get_queried_object();
+
+$first_query_params = [
+  'post_type' => 'post',
+  'posts_per_page' => 1,
+  'order' => 'DESC',
+  'orderby' => 'date',
+  'tax_query' => [
+    'relation' => 'OR',
+    [
+      'taxonomy' => $category->taxonomy,
+      'field' => 'id',
+      'terms' => [$category->term_id]
+    ]
+  ]
+];
+$first_query = new WP_Query;
+$first_posts = $first_query->query($first_query_params);
+
+$first_posts_ids = [];
+foreach ($first_posts as $post) {
+  $first_posts_ids[] = $post->ID;
+}
+
+$list_query_params = [
+  'post_type' => 'post',
+  'posts_per_page' => 3,
+  'order' => 'DESC',
+  'orderby' => 'date',
+  'paged' => get_query_var('paged') ?: 1,
+  'post__not_in' => $first_posts_ids,
+  'tax_query' => [
+    'relation' => 'OR',
+    [
+      'taxonomy' => $category->taxonomy,
+      'field' => 'id',
+      'terms' => [$category->term_id]
+    ]
+  ]
+];
+$list_query = new WP_Query($list_query_params);
 ?>
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?> itemscope itemtype="http://schema.org/WebSite">
@@ -33,28 +57,21 @@
       <main class="main">
         <div class="container">
           <div class="articles-list" id="more-articles-list">
-            <?php $idx = 0;
-            while (have_posts()):
-              the_post();
-              $idx++;
-              if ($idx === 1): ?>
-                <div class="articles-list__item articles-list__item_large">
-                  <?php get_template_part('partials/article', 'large') ?>
-                </div>
-              <?php else: ?>
-                <div class="articles-list__item">
-                  <?php get_template_part('partials/article', 'medium') ?>
-                </div>
-              <?php endif; endwhile ?>
+            <?php while ($list_query->have_posts()):
+              $list_query->the_post(); ?>
+              <div class="articles-list__item">
+                <?php get_template_part('partials/article', 'medium') ?>
+              </div>
+            <?php endwhile; ?>
           </div>
 
           <div class="articles-pagination">
-            <?php if ($wp_query->max_num_pages > 1): ?>
+            <?php if ($list_query->max_num_pages > 1): ?>
               <script>
                 var ajaxurl = '<?php echo site_url(); ?>/wp-admin/admin-ajax.php';
-                var posts_vars = '<?php echo serialize($wp_query->query_vars); ?>';
+                var posts_vars = '<?php echo serialize($list_query->query_vars); ?>';
                 var current_page = <?php echo (get_query_var('paged')) ? get_query_var('paged') : 1; ?>;
-                var max_pages = '<?php echo $wp_query->max_num_pages; ?>';
+                var max_pages = '<?php echo $list_query->max_num_pages; ?>';
               </script>
               <button id="more-articles-button" class="articles-pagination__show-more">Показать ещё</button>
             <?php endif; ?>
@@ -63,6 +80,8 @@
               <?php echo paginate_links([
                 'prev_text' => '<',
                 'next_text' => '>',
+                'current' => max(1, get_query_var('paged')),
+                'total' => $list_query->max_num_pages,
               ]) ?>
             </div>
           </div>
