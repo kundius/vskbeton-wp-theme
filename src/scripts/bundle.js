9e452ce4d93a17f8b65e119dfc0d4bc80f59aa60
Kundius Ruslan <kundius.ruslan@gmail.com>
2024-02-22 12:49:30 +0000

    deploy

--- src/scripts/bundle.js
+++ src/scripts/bundle.js
@@ -98,8 +98,8 @@ new Swiper(".block-partners .swiper", {
     rows: 3,
   },
   freeMode: true,
-  loop: true,
-  loopAddBlankSlides: true,
+  loop: false,
+  // loopAddBlankSlides: false,
   navigation: {
     nextEl: ".block-partners .slider-nav__next",
     prevEl: ".block-partners .slider-nav__prev",
