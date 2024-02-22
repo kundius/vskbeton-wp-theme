8d00f1cacb8aa5787ba78923644f879de1138072
Kundius Ruslan <kundius.ruslan@gmail.com>
2024-02-22 11:58:05 +0000

    deploy

--- blocks/partners/template.php
+++ blocks/partners/template.php
@@ -14,9 +14,17 @@ $items = get_field('items');
 			<div class="swiper">
 				<div class="swiper-wrapper">
 					<?php foreach ($items as $key => $item): ?>
+						<?php if ($key % 7 === 6): ?>
+							<div class="swiper-slide partners-slide partners-slide_before-<?php echo ($key % 7) ?>">
+							</div>
+						<?php endif ?>
 						<div class="swiper-slide partners-slide partners-slide_<?php echo ($key % 7) ?>">
 							<img class="partners-slide__image" src="<?php echo $item['image']['url'] ?>">
 						</div>
+						<?php if ($key % 7 === 0): ?>
+							<div class="swiper-slide partners-slide partners-slide_after-<?php echo ($key % 7) ?>">
+							</div>
+						<?php endif ?>
 					<?php endforeach ?>
 				</div>
 			</div>
