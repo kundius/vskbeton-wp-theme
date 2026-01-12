baed29d0a3b2d5684adcd4dc0fe8d9ac1e923c2c
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-01-12 13:07:00 +0300

    upd

--- src/scripts/geography.js
+++ src/scripts/geography.js
@@ -1,16 +1,21 @@
 import EmblaCarousel from "embla-carousel";
 import { addPrevNextBtnsClickHandlers } from "./EmblaCarouselArrowButtons";
+import ClassNames from "embla-carousel-class-names";
 
 export function applyGeography(wrap) {
   const viewportNode = wrap.querySelector("[data-geography-viewport]");
   const prevNode = wrap.querySelector("[data-geography-prev]");
   const nextNode = wrap.querySelector("[data-geography-next]");
 
-  const emblaApi = EmblaCarousel(viewportNode, {
-    loop: true,
-    slidesToScroll: 1,
-    containScroll: false,
-  });
+  const emblaApi = EmblaCarousel(
+    viewportNode,
+    {
+      loop: true,
+      slidesToScroll: 1,
+      containScroll: false,
+    },
+    [ClassNames()]
+  );
 
   const removeMainPrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
     emblaApi,
