9d377ef0e9ed40e2007d79f414b2b41a33d91c4f
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-01-14 14:41:26 +0300

    upd

--- template-products.php
+++ template-products.php
@@ -34,8 +34,43 @@ $productsQuery = new WP_Query($args);
         <div class="page-content content">
           <?php the_content(); ?>
         </div>
+      </div>
+    </div>
 
-        <?php if ($productsQuery->have_posts()): ?>
+    <?php if ($products = get_field("products", 2)): ?>
+      <section class="products">
+        <div class="container">
+          <div class="products__title">
+            Наша продукция
+          </div>
+          <div class="products-grid">
+            <?php foreach ($products['list'] as $item): ?>
+              <div class="products-grid__cell">
+                <div class="products-item products-item--<?php echo $item['color']; ?>">
+                  <div class="products-item__title">
+                    <?php echo $item['name']; ?>
+                  </div>
+                  <a href="<?php echo $item['link_url']; ?>" class="more-link">
+                    <span><?php echo $item['link_text']; ?></span>
+                    <span></span>
+                  </a>
+                  <?php if ($icon = $item['icon']): ?>
+                    <img src="<?php echo $icon['url']; ?>" class="products-item__image">
+                  <?php endif; ?>
+                </div>
+              </div>
+            <?php endforeach; ?>
+          </div>
+        </div>
+      </section>
+    <?php endif; ?>
+
+    <?php if ($productsQuery->have_posts()): ?>
+      <section class="services">
+        <div class="container">
+          <div class="services__title-alt">
+            НАШИ УСЛУГИ
+          </div>
           <div class="services-grid">
             <?php while ($productsQuery->have_posts()): $productsQuery->the_post(); ?>
               <div class="services-grid__cell">
@@ -52,10 +87,10 @@ $productsQuery = new WP_Query($args);
               </div>
             <?php endwhile; ?>
           </div>
-        <?php endif; ?>
-        <?php wp_reset_postdata(); ?>
-      </div>
-    </div>
+        </div>
+      </section>
+    <?php endif; ?>
+    <?php wp_reset_postdata(); ?>
 
     <?php get_template_part("partials/footer"); ?>
   </div>
