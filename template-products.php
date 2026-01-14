47d3583699620059f778a2e4119bd03079049a50
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-01-14 14:16:26 +0300

    upd

--- template-products.php
+++ template-products.php
@@ -6,67 +6,67 @@ Template Name: Продукция и услуги
 global $post;
 
 $args = [
-    "post_type" => "page",
-    "post_parent" => $post->ID,
+  "post_type" => "page",
+  "post_parent" => $post->ID,
 ];
 
 $productsQuery = new WP_Query($args);
 ?>
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?> itemscope itemtype="http://schema.org/WebSite">
-  <head>
-    <?php get_template_part("partials/head"); ?>
-  </head>
-  <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
 
-    <div class="page">
-      <?php get_template_part("partials/header"); ?>
+<head>
+  <?php get_template_part("partials/head"); ?>
+</head>
 
-      <div class="main">
-        <div class="container">
-			<div class="page-headline">
-					<h1 class="page-headline__title"><?php the_title(); ?></h1>
-				</div>
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
 
-          <?php
-          if ($productsQuery->have_posts()): ?>
-          <div class="services">
-            <div class="services-grid">
-              <?php
-              $key = 0;
-              while ($productsQuery->have_posts()):
+  <div class="page">
+    <?php get_template_part("partials/header"); ?>
 
-                  $productsQuery->the_post();
-                  $key++;
-                  ?>
-              <div class="services-grid__cell<?php if (
-                  $productsQuery->post_count % 2 > 0 &&
-                  $key === $productsQuery->post_count
-              ): ?> services-grid__cell_wide<?php endif; ?>">
-                <div class="simple-card">
-                  <div class="simple-card__image">
-                    <?php the_post_thumbnail("article-medium"); ?>
-                  </div>
-                  <a href="<?php the_permalink(); ?>" class="simple-card__title">
-                    <?php echo get_field("title_in_list") ?: get_the_title(); ?>
-                  </a>
-                </div>
-              </div>
-              <?php
-              endwhile;
-              ?>
-            </div>
-          </div>
-          <?php endif;
-          wp_reset_postdata();
-          ?>
+    <div class="main">
+      <div class="container">
+        <div class="page-headline">
+          <h1 class="page-headline__title"><?php the_title(); ?></h1>
         </div>
       </div>
-
-      <?php get_template_part("partials/footer"); ?>
     </div>
 
-    <?php wp_footer(); ?>
-  </body>
+    <?php if ($productsQuery->have_posts()): ?>
+      <section class="services">
+        <div class="container">
+          <div class="services__title">
+            <span>
+              <strong>НАШИ</strong><br>
+              УСЛУГИ
+            </span>
+          </div>
+          <div class="services-grid">
+            <?php while ($productsQuery->have_posts()): $productsQuery->the_post(); ?>
+              <div class="services-grid__cell">
+                <div class="services-item">
+                  <div class="services-item__title">
+                    <?php echo (get_field("title_in_list") ?: get_the_title()); ?>
+                  </div>
+                  <a href="<?php the_permalink(); ?>" class="more-link">
+                    <span>Подробнее</span>
+                    <span></span>
+                  </a>
+                  <div class="services-item__space"></div>
+                </div>
+              </div>
+            <?php endwhile; ?>
+          </div>
+        </div>
+      </section>
+    <?php endif; ?>
+    <?php wp_reset_postdata(); ?>
+
+    <?php get_template_part("partials/footer"); ?>
+  </div>
+
+  <?php wp_footer(); ?>
+</body>
+
 </html>
