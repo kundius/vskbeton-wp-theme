8c4a691321642e2f67745831e174494b01d7cd80
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-01-14 14:17:43 +0300

    upd

--- template-products.php
+++ template-products.php
@@ -29,31 +29,31 @@ $productsQuery = new WP_Query($args);
       <div class="container">
         <div class="page-headline">
           <h1 class="page-headline__title"><?php the_title(); ?></h1>
-
-          <div class="page-content content">
-            <?php the_content(); ?>
-          </div>
-
-          <?php if ($productsQuery->have_posts()): ?>
-            <div class="services-grid">
-              <?php while ($productsQuery->have_posts()): $productsQuery->the_post(); ?>
-                <div class="services-grid__cell">
-                  <div class="services-item">
-                    <div class="services-item__title">
-                      <?php echo (get_field("title_in_list") ?: get_the_title()); ?>
-                    </div>
-                    <a href="<?php the_permalink(); ?>" class="more-link">
-                      <span>Подробнее</span>
-                      <span></span>
-                    </a>
-                    <div class="services-item__space"></div>
-                  </div>
-                </div>
-              <?php endwhile; ?>
-            </div>
-          <?php endif; ?>
-          <?php wp_reset_postdata(); ?>
         </div>
+
+        <div class="page-content content">
+          <?php the_content(); ?>
+        </div>
+
+        <?php if ($productsQuery->have_posts()): ?>
+          <div class="services-grid">
+            <?php while ($productsQuery->have_posts()): $productsQuery->the_post(); ?>
+              <div class="services-grid__cell">
+                <div class="services-item">
+                  <div class="services-item__title">
+                    <?php echo (get_field("title_in_list") ?: get_the_title()); ?>
+                  </div>
+                  <a href="<?php the_permalink(); ?>" class="more-link">
+                    <span>Подробнее</span>
+                    <span></span>
+                  </a>
+                  <div class="services-item__space"></div>
+                </div>
+              </div>
+            <?php endwhile; ?>
+          </div>
+        <?php endif; ?>
+        <?php wp_reset_postdata(); ?>
       </div>
     </div>
 
