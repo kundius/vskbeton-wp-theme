9c718cc14f9f003628c3b043e1f762cafbc14098
Kundius Ruslan <kundius.ruslan@gmail.com>
2026-03-05 23:59:32 +0300

    Update seo.php

--- inc/seo.php
+++ inc/seo.php
@@ -68,6 +68,61 @@ function sitemap_news_activation() {
 	}
 }
 
+function add_meta() {
+	$title = '';
+	$description = '';
+	$keywords = '';
+
+	if (is_archive()) {
+		$term = \get_term_by('slug', \get_query_var('term'), \get_query_var('taxonomy'));
+		if ($term) {
+			$title = \get_field('title', $term->taxonomy . '_' . $term->term_id);
+			if (empty($title)) {
+				$title = $term->name;
+			}
+			$description = \get_field('seo_description', $term->taxonomy . '_' . $term->term_id);
+			$keywords = \get_field('seo_keywords', $term->taxonomy . '_' . $term->term_id);
+		} elseif (\is_post_type_archive()) {
+			$title = \get_queried_object()->labels->name;
+		} elseif (\is_day()) {
+			$title = sprintf(__('Daily Archives: %s', 'roots'), \get_the_date());
+		} elseif (\is_month()) {
+			$title = sprintf(__('Monthly Archives: %s', 'roots'), \get_the_date('F Y'));
+		} elseif (\is_year()) {
+			$title = sprintf(__('Yearly Archives: %s', 'roots'), \get_the_date('Y'));
+		} elseif (\is_author()) {
+			$author = \get_queried_object();
+			$title = sprintf(__('Author Archives: %s', 'roots'), $author->display_name);
+		} else {
+			$title = \single_cat_title('', false);
+		}
+	} elseif (\is_search()) {
+		$title = sprintf(__('Search Results for %s', 'roots'), \get_search_query());
+	} elseif (\is_404()) {
+		$title = 'Not Found';
+	} else {
+		$title = \get_field('seo_title');
+		if (empty($title)) {
+			$title = \get_the_title();
+		}
+		$description = \get_field('seo_description');
+		$keywords = \get_field('seo_keywords');
+	}
+
+	if (!empty($title)) {
+		echo '<title>' . $title . '</title>';
+	}
+
+	if (!empty($keywords)) {
+		echo '<meta name="keywords" content="' . $keywords . '">';
+	}
+
+	if (!empty($description)) {
+		echo '<meta name="description" content="' . $description . '">';
+	}
+}
+add_action('wp_head', 'add_meta');
+
 add_action('sitemap_news_hourly_event', 'do_this_hourly');
 function do_this_hourly() {
   if (str_replace('-', '', get_option('gmt_offset')) < 10) {
